4d192b2dc8c4cb3104cfa13df84d7895928f9b20
MMarnerou <mmarnerou@outlook.com>
2020-09-14 22:33:36 +0300

    make it better

---
+++ data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    channel: "sssssanga",
+    description: "YOOO this works!",
+    song: "99 problems but REACT AINT ONE!",
+    messages: 123,
+    likes: 456,
+    shares: 789,
+    url: "http://techslides.com/demos/sample-videos/small.ogv",
+  },
+  {
+    channel: "sssssanga",
+    description: "YOOO this works!",
+    song: "99 problems but REACT AINT ONE!",
+    messages: 123,
+    likes: 456,
+    shares: 789,
+    url: "http://techslides.com/demos/sample-videos/small.ogv",
+  },
+  {
+    channel: "sssssanga",
+    description: "YOOO this works!",
+    song: "99 problems but REACT AINT ONE!",
+    messages: 123,
+    likes: 456,
+    shares: 789,
+    url: "http://techslides.com/demos/sample-videos/small.ogv",
+  },
+];
+
+export default data;
